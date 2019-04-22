e22cd305eabc7be3b463cef29c651b19c47a4646
Anze Kolar <me@akolar.com>
2019-04-22 11:36:31 +0200

    Add .travis.yml

---
+++ build.sh
@@ -0,0 +1,26 @@
+echo "Running gofmt"
+if [ -n "$(gofmt -s -l .)" ]; then
+    echo "Code is not formatted."
+    exit 1
+else
+    echo "gofmt succeeded"
+fi
+
+echo "Running govet"
+if [ -n "$(go vet .)" ]; then 
+    echo "go vet failed."
+    exit 1
+else
+    echo "go vet succeeded"
+fi
+
+echo "Building binaries..."
+if [ -n "$(go build ./...)" ]; then 
+    echo "Build failed"
+    exit 1
+else
+    echo "Build succeeded"
+fi
+
+echo "Running tests..."
+go test -v ./...
